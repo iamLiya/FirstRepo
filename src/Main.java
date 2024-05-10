2fa6dca28e49c3646914a262caeb632572e3178d
Liya <liyabonadunga@gmai.com>
2024-05-10 14:27:49 +0200

    First Change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner (Systems.in)
+        System.out.print("Liyabona");
+        String name = input.next();
+                System.out.println("Welcome" + name);
     }
 }
